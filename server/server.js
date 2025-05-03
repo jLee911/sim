29b0df11596e779559039cf029b842601eefd2ae
jLee911 <jenaleetackitt911@gmail.com>
2025-05-02 21:46:08 -0700

    Add working server.js and package.json for multiplayer backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,36 @@
-// Colyseus server file goes here
+
+const colyseus = require("colyseus");
+const http = require("http");
+const express = require("express");
+const { Server } = require("colyseus");
+const { Room } = require("colyseus");
+
+const app = express();
+const gameServer = new Server({
+  server: http.createServer(app)
+});
+
+class GoatRoom extends Room {
+  onCreate() {
+    this.setState({ players: {} });
+
+    this.onMessage("scream", (client) => {
+      this.broadcast("scream", null, { except: client });
+    });
+  }
+
+  onJoin(client) {
+    this.state.players[client.sessionId] = { x: 0, y: 0 };
+    this.send(client, "joined", { id: client.sessionId });
+  }
+
+  onLeave(client) {
+    delete this.state.players[client.sessionId];
+  }
+
+  onDispose() {}
+}
+
+gameServer.define("goats", GoatRoom);
+gameServer.listen(2567);
+console.log("Colyseus Goat server running on ws://localhost:2567");
